7fb29dd672aa627af7115af5f1ed4b11cf9669c3
虹オフィス <noreply@nijioffice.local>
2026-09-19 03:16:44 +0900

    起動時に同じブラウザで2ページを開く修正版

--- start-tool.cmd
+++ start-tool.cmd
@@ -16,14 +16,15 @@ if errorlevel 1 (
 
 :server_ready
 
-set "BROWSER_EXE=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
-if not exist "%BROWSER_EXE%" set "BROWSER_EXE=%LocalAppData%\Google\Chrome\Application\chrome.exe"
-if not exist "%BROWSER_EXE%" set "BROWSER_EXE=%ProgramFiles%\Microsoft\Edge\Application\msedge.exe"
-if exist "%BROWSER_EXE%" goto browser_found
+set "BROWSER_EXE="
+for %%P in ("%ProgramFiles%\Google\Chrome\Application\chrome.exe" "%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe" "%LocalAppData%\Google\Chrome\Application\chrome.exe" "%ProgramFiles%\Microsoft\Edge\Application\msedge.exe" "%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe" "%LocalAppData%\Microsoft\Edge\Application\msedge.exe") do if not defined BROWSER_EXE if exist "%%~P" set "BROWSER_EXE=%%~P"
+if defined BROWSER_EXE goto browser_found
 goto fallback_browser
 
 :browser_found
-start "" "%BROWSER_EXE%" --remote-debugging-port=9222 --user-data-dir="%~dp0browser-profile" "http://127.0.0.1:4173/" "https://coconala.com/mypage/analytics"
+start "" "%BROWSER_EXE%" --remote-debugging-port=9222 --user-data-dir="%~dp0browser-profile" --new-window "http://127.0.0.1:4173/"
+timeout /t 2 /nobreak >nul
+start "" "%BROWSER_EXE%" --remote-debugging-port=9222 --user-data-dir="%~dp0browser-profile" --new-tab "https://coconala.com/mypage/analytics"
 goto done
 
 :fallback_browser
